bd36d7735881da9a4f61fc2018fece620687f409
DmitryPatskalev <dimanord@yandex.ru>
2022-07-13 12:14:19 +0300

    commit

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import css from './App.module.css'
 
 import HW1 from "../../../p2-homeworks/h1/HW1";
 import HW2 from "../../../p2-homeworks/h2/HW2";
@@ -14,7 +15,7 @@ function App() {
           <HW2/>
           <HW3/>
           <HW4/>
-          {/*<HW5/>*/}
+          <HW5/>
       </div>
     );
 }
