ac46e1119aa24e1c6f6f8a2e1321eab40ba51f87
DmitryPatskalev <dimanord@yandex.ru>
2023-02-01 14:45:42 +0300

    HW8_Part1

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            if (action.payload === 'up') {
+                return state.map(el => el).sort((a, b) => a > b ? 1 : -1)
+            }
+            if (action.payload === 'down') {
+                return state.map(el => el).sort((a, b) => a < b ? 1 : -1)
+            }
+            return state
 
-            return state // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age > action.payload)
         }
         default:
             return state
