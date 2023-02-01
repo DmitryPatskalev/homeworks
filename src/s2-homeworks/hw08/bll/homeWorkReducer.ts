f64194570840256ecdbbb9de08e6c846b7b635de
DmitryPatskalev <dimanord@yandex.ru>
2023-02-01 17:31:50 +0300

    HW8_Done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,10 +8,10 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
             if (action.payload === 'up') {
-                return state.map(el => el).sort((a, b) => a > b ? 1 : -1)
+                return  [...state].sort((a, b) => a.name > b.name ? 1 : -1)
             }
             if (action.payload === 'down') {
-                return state.map(el => el).sort((a, b) => a < b ? 1 : -1)
+                return [...state].sort((a, b) => a.name < b.name ? 1 : -1)
             }
             return state
 
