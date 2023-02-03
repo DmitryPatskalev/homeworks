66f8f64238d681cf4a0658d73dd8a78a3292835c
DmitryPatskalev <dimanord@yandex.ru>
2023-02-03 16:55:18 +0300

    HW9_Done

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -4,102 +4,103 @@ import {restoreState} from '../hw06/localStorage/localStorage'
 import s from './Clock.module.css'
 
 function Clock() {
-    const [timerId, setTimerId] = useState<number | undefined>(undefined)
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
-    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
-    const [show, setShow] = useState<boolean>(false)
-    const [disabled, setIsDisabled] = useState(false)
+	 const [timerId, setTimerId] = useState<number | undefined>(undefined)
+	 // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
+	 const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
+	 const [show, setShow] = useState<boolean>(false)
+	 const [disabled, setIsDisabled] = useState(false)
 
-    const days = ['', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    const month =
-        ['January', 'February', 'March',
-            'April', 'May', 'June',
-            'July', 'August', 'September',
-            'October', 'November', 'December',
-        ]
-    let timer: ReturnType<typeof setInterval>
-    const start = () => {
-        timer = setInterval(() => {
-            setDate(new Date())
-        }, 1000)
-        setIsDisabled(!disabled)
-    }
+	 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+	 const month =
+		 ['January', 'February', 'March', 'April', 'May', 'June',
+				'July', 'August', 'September', 'October', 'November', 'December',
+		 ]
+	 const start = () => {
+			const id: number = Number(setInterval(() => {
+				 setDate(new Date())
+			}, 1000))
+			setTimerId(id)
+			setIsDisabled(!disabled)
+	 }
 
-    // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
-    // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
+	 // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
+	 // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
 
 
-    const stop = () => {// пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-        clearInterval(timer)
-        setIsDisabled(!disabled)
-    }
+	 const stop = () => {// пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
+			if (timerId !== undefined) {
+				 clearInterval(timerId)
+				 setIsDisabled(!disabled)
+			}
+			setTimerId(undefined)
+	 }
 
-    const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-        setShow(true)
-    }
-    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-        setShow(false)
-    }
+	 const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
+			setShow(true)
+	 }
+	 const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
+			setShow(false)
+	 }
 
-    const get2Digit = (num: number) => num < 10 ? '0' + num : num
-    const stringTime = `${get2Digit(date.getHours())}:${get2Digit(date.getMinutes())}:${get2Digit(date.getSeconds())}` ||
-        <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = month[date.getMonth()] || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+	 const get2Digit = (num: number) => num < 10 ? '0' + num : num
+	 const stringTime = `${get2Digit(date.getHours())}:${get2Digit(date.getMinutes())}:${get2Digit(date.getSeconds())}` ||
+     <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+	 const stringDate = month[date.getMonth()] || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = days[date.getDay()] || <br/> // пишут студенты
-    const stringMonth = `${get2Digit(date.getDate())}.${get2Digit(date.getMonth() + 1)}.${get2Digit(date.getFullYear())}` ||
-        <br/> // пишут студенты
+	 // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
+	 const stringDay = days[date.getDay() - 1] || <br/> // пишут студенты
+	 const stringMonth = `${get2Digit(date.getDate())}.${get2Digit(date.getMonth() + 1)}.${get2Digit(date.getFullYear())}` ||
+     <br/> // пишут студенты
 
-    return (
-        <div className={s.clock}>
-            <div
-                id={'hw9-watch'}
-                className={s.watch}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                <span id={'hw9-day'}>{stringDay}</span>,{' '}
-                <span id={'hw9-time'}>
+	 return (
+		 <div className={s.clock}>
+				<div
+					id={'hw9-watch'}
+					className={s.watch}
+					onMouseEnter={onMouseEnter}
+					onMouseLeave={onMouseLeave}
+				>
+					 <span id={'hw9-day'}>{stringDay}</span>,{' '}
+					 <span id={'hw9-time'}>
                     <strong>{stringTime}</strong>
                 </span>
-            </div>
+				</div>
 
-            <div id={'hw9-more'}>
-                <div className={s.more}>
-                    {show ? (
-                        <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDate}</span>
-                        </>
-                    ) : (
-                        <>
-                            <br/>
-                        </>
-                    )}
-                </div>
-            </div>
+				<div id={'hw9-more'}>
+					 <div className={s.more}>
+							{show ? (
+								<>
+									 <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+									 <span id={'hw9-date'}>{stringDate}</span>
+								</>
+							) : (
+								<>
+									 <br/>
+								</>
+							)}
+					 </div>
+				</div>
 
-            <div className={s.buttonsContainer}>
-                <SuperButton
-                    id={'hw9-button-start'}
-                    disabled={disabled}// пишут студенты // задизэйблить если таймер запущен
-                    onClick={start}
-                    xType={disabled ? 'disabled' : 'default'}
-                >
-                    start
-                </SuperButton>
-                <SuperButton
-                    id={'hw9-button-stop'}
-                    disabled={!disabled} // пишут студенты // задизэйблить если таймер не запущен
-                    onClick={stop}
-                    xType={disabled ? 'default' : 'disabled'}
-                >
-                    stop
-                </SuperButton>
-            </div>
-        </div>
-    )
+				<div className={s.buttonsContainer}>
+					 <SuperButton
+						 id={'hw9-button-start'}
+						 disabled={disabled}// пишут студенты // задизэйблить если таймер запущен
+						 onClick={start}
+						 xType={disabled ? 'disabled' : 'default'}
+					 >
+							start
+					 </SuperButton>
+					 <SuperButton
+						 id={'hw9-button-stop'}
+						 disabled={!disabled} // пишут студенты // задизэйблить если таймер не запущен
+						 onClick={stop}
+						 xType={disabled ? 'default' : 'disabled'}
+					 >
+							stop
+					 </SuperButton>
+				</div>
+		 </div>
+	 )
 }
 
 export default Clock
