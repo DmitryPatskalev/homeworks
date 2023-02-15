cb77b87b3b1c7a3a91a7fa7f97475deb2b920153
DmitryPatskalev <dimanord@yandex.ru>
2023-02-15 19:08:57 +0300

    HW15_Part2

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,54 +1,58 @@
 import React from 'react'
+import down from './../../img/Polygon 5.svg'
+import up from './../../img/Polygon 2.svg'
+import s from './../../HW15.module.css'
 
 // добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+
+
+const downIcon = down
+const upIcon = up
+const noneIcon = '[]'
 
 export type SuperSortPropsType = {
-    id?: string
-    sort: string
-    value: string
-    onChange: (newSort: string) => void
+	 id?: string
+	 sort: string
+	 value: string
+	 onChange: (newSort: string) => void
 }
 
 export const pureChange = (sort: string, down: string, up: string) => {
-    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+	 return sort === down
+		 ? up
+		 : sort === up
+			 ? ''
+			 : down
+
+	 // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
+// исправить
 }
 
 const SuperSort: React.FC<SuperSortPropsType> = (
-    {
-        sort, value, onChange, id = 'hw15',
-    }
+	{
+		 sort, value, onChange, id = 'hw15',
+	}
 ) => {
-    const up = '0' + value
-    const down = '1' + value
+	 const up = '0' + value
+	 const down = '1' + value
 
-    const onChangeCallback = () => {
-        onChange(pureChange(sort, down, up))
-    }
+	 const onChangeCallback = () => {
+			onChange(pureChange(sort, down, up))
+	 }
 
-    const icon = sort === down
-        ? downIcon
-        : sort === up
-            ? upIcon
-            : noneIcon
+	 const icon = sort === down
+		 ? downIcon
+		 : sort === up
+			 ? upIcon
+			 : noneIcon
 
-    return (
-        <span
-            id={id + '-sort-' + value}
-            onClick={onChangeCallback}
-        >
-            {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
 
-            {icon} {/*а это убрать*/}
-        </span>
-    )
+	 return (
+		 <span className={s.iconContainer} id={id + '-sort-' + value} onClick={onChangeCallback}>
+            <img id={id + '-icon-' + sort} src={upIcon} alt={''}/>
+            <img id={id + '-icon-' + sort} src={downIcon} alt={''}/>
+		 </span>
+	 )
 }
 
 export default SuperSort
--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -1,67 +1,66 @@
-import React, {ChangeEvent} from 'react'
+import React from 'react'
 import SuperSelect from '../../../hw07/common/c5-SuperSelect/SuperSelect'
 import {Pagination} from '@mui/material'
 import s from './SuperPagination.module.css'
 
 export type SuperPaginationPropsType = {
-    id?: string
-    page: number
-    itemsCountForPage: number
-    totalCount: number
-    onChange: (page: number, count: number) => void
+	 id?: string
+	 page: number
+	 itemsCountForPage: number
+	 totalCount: number
+	 onChange: (page: number, count: number) => void
 }
 
 const SuperPagination: React.FC<SuperPaginationPropsType> = (
-    {
-        page, itemsCountForPage, totalCount, onChange, id = 'hw15',
-    }
+	{
+		 page, itemsCountForPage, totalCount, onChange, id = 'hw15',
+	}
 ) => {
-    const lastPage = 10 // пишет студент // вычислить количество страниц
+	 const lastPage = Math.ceil(totalCount / page) // пишет студент // вычислить количество страниц
 
-    const onChangeCallback = (event: any, page: number) => {
-       onChange(event.currentTarget.value, page)
-        // пишет студент
-    }
+	 const onChangeCallback = (event: any, page: number) => {
+			onChange(page, itemsCountForPage)
+			// пишет студент
+	 }
 
-    const onChangeSelect = (event: any) => {
-       onChange(event.currentTarget.value, totalCount)
-        // пишет студент
-    }
+	 const onChangeSelect = (event: any) => {
+			onChange(page, Number(event.currentTarget.value))
+			// пишет студент
+	 }
 
-    return (
-        <div className={s.pagination}>
-            <Pagination
-                id={id + '-pagination'}
-                sx={{
-                    // стили для Pagination // пишет студент
-                }}
-                page={page}
-                count={lastPage}
-                onChange={onChangeCallback}
-                hideNextButton
-                hidePrevButton
-            />
+	 return (
+		 <div className={s.pagination}>
+				<Pagination
+					id={id + '-pagination'}
+					page={page}
+					count={lastPage}
+					onChange={onChangeCallback}
+					hideNextButton
+					hidePrevButton
+				/>
 
-            <span className={s.text1}>
-                показать
+				<span className={s.text1}>
+                Показать
             </span>
 
-            <SuperSelect
-                id={id + '-pagination-select'}
-                value={itemsCountForPage}
-                options={[
-                    {id: 4, value: 4},
-                    {id: 7, value: 7},
-                    {id: 10, value: 10},
-                ]}
-                onChange={onChangeSelect}
-            />
+				<SuperSelect
+					id={id + '-pagination-select'}
+					value={itemsCountForPage}
+					options={[
+						 {id: 4, value: 4},
+						 {id: 7, value: 7},
+						 {id: 10, value: 10},
+					]}
+					onChange={onChangeSelect}
+					className={s.select}
 
-            <span className={s.text2}>
+				/>
+
+				<span className={s.text2}>
                 строк в таблице
             </span>
-        </div>
-    )
+		 </div>
+	 )
 }
 
 export default SuperPagination
